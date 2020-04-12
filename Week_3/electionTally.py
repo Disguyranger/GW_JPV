363632f16ce8521aa5abab39edd908902349f0d4
Disguyranger <46687995+Disguyranger@users.noreply.github.com>
2020-04-11 22:58:21 -0400

    all missing data required for submission completed.

---
+++ Week_3/electionTally.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 11 03:26:55 2020
+
+@author: disguy
+"""
+import time, sys, os
+
+
+def storyPrint(text, timer):
+    for letter in text:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(timer)
+    print("\n")
+
+
+def votesCounter(inputPath):
+    totalVotes = 0
+    totalVotesKhan = 0
+    totalVotesCorrey = 0
+    totalVotesLi = 0
+    totalVotesTooley = 0
+    winner = ''
+    # counter = 0
+    with open(inputPath, 'r') as f:
+        next(f)
+        
+        for row in f:
+            # counter += 1
+            # if counter == 10: 
+            #     break
+            
+            totalVotes += 1
+            row = row.split(",")
+            
+            if row[2] == 'Khan\n':
+                totalVotesKhan += 1
+            elif str(row[2]) == 'Correy\n':
+                totalVotesCorrey += 1
+            elif str(row[2]) == 'Li\n':
+                totalVotesLi += 1
+            elif str(row[2]) == "O'Tooley\n":
+                totalVotesTooley += 1
+            else:
+                continue
+        if totalVotesKhan > totalVotesCorrey and totalVotesKhan > totalVotesLi and totalVotesKhan > totalVotesTooley:
+            winner = 'Khan'
+            
+        elif totalVotesCorrey > totalVotesKhan and totalVotesCorrey > totalVotesLi and totalVotesCorrey > totalVotesTooley:
+            winner = 'Correy'
+        elif totalVotesLi > totalVotesKhan and totalVotesLi > totalVotesCorrey and totalVotesLi > totalVotesTooley: 
+            winner = "Li" 
+        else:
+            winner = "O'Tooley"
+    
+    
+    return(totalVotes, totalVotesKhan, totalVotesCorrey, totalVotesLi, totalVotesTooley, winner)
+
+def printerWriter(totalVotes, totalVotesKhan, totalVotesCorrey, totalVotesLi, totalVotesTooley, winner, inputPath):
+    f = open('election_data_.txt', 'w')
+
+    
+    pKhan = totalVotesKhan / totalVotes
+    pCorrey =  totalVotesCorrey / totalVotes
+    pLi = totalVotesLi / totalVotes
+    pTool = totalVotesTooley / totalVotes
+    
+    outputToConsole = f"""
+       Election Results
+----------------------------------
+Total Votes:   {totalVotes:,}
+----------------------------------
+Khan: {pKhan:.3f}%  {totalVotesKhan:,}
+Correy: {pCorrey:.3f}%  {totalVotesCorrey:,}
+Li: {pLi:.3f}%  {totalVotesLi:,}
+O'Tooley': {pTool:.3f}%  {totalVotesTooley:,}
+----------------------------------
+Winner: {winner}
+----------------------------------
+"""
+
+    f.write(outputToConsole)
+    storyPrint(outputToConsole, .075)
+    f.close()
+def main():
+    inputPath = os.path.join('Resources', 'election_data.csv')
+
+    totalVotes, totalVotesKhan, totalVotesCorrey, totalVotesLi, totalVotesTooley, winner = votesCounter(inputPath)
+    printerWriter(totalVotes, totalVotesKhan, totalVotesCorrey, totalVotesLi, totalVotesTooley, winner, inputPath)
+    try:
+        os.remove( './election_Data_Totals.txt')
+        a = 'Output File already exists. Deleting...'
+        storyPrint(a, .075)
+    except OSError:
+        b = 'Creating File Python_Write_File.txt...'
+        storyPrint(b, .075)
+        pass
+    
+        
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+        
---
+++ Week_3/main.py
@@ -0,0 +1,81 @@
+"""
+Created on Fri Apr 10 01:19:04 2020
+
+@author: disguy
+"""
+import time, sys, os
+
+def storyPrint(text, timer):
+    for letter in text:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(timer)
+    print("\n")
+
+def greatestGainAndLoss(inputPath):
+    previousRow = None
+    currentRow = None
+    greatestGain = {"value": 0, "date": ""}
+    greatestLoss = {"value": 0, "date": ""}
+    totalMonths = 0
+    totalProfits = 0
+    totalChange = 0
+    with open(inputPath, 'r') as f:
+        next(f)
+        
+        for row in f:
+            totalMonths += 1
+            
+            row = row.split(',')
+            totalProfits += int(row[1])
+            if previousRow is None:
+                previousRow = row
+                continue
+            
+            currentRow = row
+    
+            currentDelta = int(currentRow[1]) - int(previousRow[1])
+            totalChange += currentDelta
+    
+            if currentDelta < 0 and currentDelta < greatestLoss["value"]:
+                greatestLoss["value"] = currentDelta
+                greatestLoss["date"] = currentRow[0]
+            elif currentDelta > 0 and currentDelta > greatestGain["value"]:
+                greatestGain["value"] = currentDelta
+                greatestGain["date"] = currentRow[0]
+
+            previousRow = currentRow
+    
+    return(greatestLoss, greatestGain, totalMonths, totalProfits, totalChange/(totalMonths - 1))
+
+def printerWriter(grtLoss, grtGain, totalMonths, totalProfits, averageChange, inputPath):
+    f = open('Python_Write_File.txt', 'w')
+
+    outputToConsole = f"""
+       Financial Analysis
+----------------------------------
+Total Months:   {totalMonths}
+Total:   ${totalProfits:,}
+Average Change:   $ {averageChange:,.2f}
+Greatest Increase in Profits: {grtGain['date']} $ {grtGain['value']:,.2f}
+Greatest Decrease in Profits: {grtLoss['date']} ($ {grtLoss['value']:,.2f})
+"""
+    f.write(outputToConsole)
+    storyPrint(outputToConsole, .075)
+
+def main():
+    inputPath = os.path.join('Resources', 'budget_data.csv')
+
+    grtLoss, grtGain, totalMonths, totalProfits, averageChange = greatestGainAndLoss(inputPath)
+
+    try:
+        os.remove( './Budget_Data_Totals.txt')
+        a = 'Output File already exists. Deleting...'
+        storyPrint(a, .0625)
+    except OSError:
+        b = 'Creating File Python_Write_File.txt...'
+        storyPrint(b, .0625)
+        pass
+    printerWriter(grtLoss, grtGain, totalMonths, totalProfits, averageChange, inputPath)
+        
+main()
