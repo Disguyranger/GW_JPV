78e301645ab5d032366c29caae9f69713ce0b89e
Disguyranger <46687995+Disguyranger@users.noreply.github.com>
2020-05-08 16:23:05 -0400

    committing changes, will work on leftovers after

--- Week_6_Project 1/Project_1.py
+++ Week_6_Project 1/Project_1.py
@@ -10,36 +10,61 @@ import tkinter as tk
 import requests, json
 import datetime, time
 from scipy.stats import linregress
-#%%
-
-
-coin_type = 'bitcoin'#input("What coin are you searching for: ").lower()
-currency_type = 'usd'#input("What Currency type are you looking for: ")
-time_frame = '31'#int(input("How far back would you like to look (in days format): "))
-
-url = f'https://api.coingecko.com/api/v3/coins/{coin_type}/market_chart?vs_currency={currency_type}&days={time_frame}'
-response = requests.get(url)
-print(url)
-
-data_construct = response.json()
-
-
-date_time = []
-value = []
-
-for row in data_construct['prices']:
-    date_time.append(datetime.datetime.utcfromtimestamp(row[0]/1000))
-    value.append(row[1])
+import sys
+def storyPrint(text):
+    for letter in text:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(.04)
+    print("\n")
     
-plt.figure(figsize=(9,5))
-plt.xticks(rotation = 30)
-plt.grid()
-plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-plt.plot(date_time, value, "-.", color = 'purple')
-plt.title(f"Bitcoin price over {time_frame} days")
-plt.xlabel('Date')
-plt.ylabel('Price per Coin')
+    
+def dataFrameCreation(coin_type, currency_type, time_frame):
+    date_time = []
+    value = []
+    
+    url = f'https://api.coingecko.com/api/v3/coins/{coin_type}/market_chart?vs_currency={currency_type}&days={time_frame}'
+    response = requests.get(url)
+    data_construction = response.json()
+    if data_construction == {"error":"Could not find coin with the given id"}:
+        fail = (f"You goofed.\nThe Cryptocoin: {coin_type} doesn't exist. \nPlease try again with the correct parameters.")
+        storyPrint(fail)
+    elif data_construction == {'error': 'invalid vs_currency'}:
+        fail = (f"You goofed.\nThe Currency type: {currency_type} doesn't exist. \nPlease try again with the correct parameters.")
+        storyPrint(fail)
+    else:
+        for row in data_construction['prices']:
+            date_time.append(datetime.datetime.utcfromtimestamp(row[0]/1000))
+            value.append(row[1])
+    
+    return(date_time, value)
+
+def dataFrameBaby(date_time, value):
+    date_time = date_time.split(' ')
+    date = date_time[0]
+    time = date_time[1]
+    print(date, time)
+
+def main():
+    quickKeys = {'1': 'bitcoin', '2': 'litecoin', '3': 'ethereum'}
+    coin_type = input("""What coin are you searching for? \n Here is a quick list in case you find yourself in a bind --[1] for Bitcoin (BTC), [2] for Litecoin (LTC), [3] Ethereum (ETH): """).lower()
+    if coin_type == '1' or coin_type == "2" or coin_type == '3':
+        coin_type = quickKeys[coin_type]
+    currency_type = input('What currency type are you looking for: ').lower()
+    
+    while True:
+        try:
+            time_frame = int(input("How far back would you like to look (in days format): "))
+            break
+        except ValueError:
+            print('Value is not an integer. Please, try again.')
+        
+
+    notFail = (f"You are looking for {coin_type.capitalize()}'s price, in {currency_type.upper()} in a time frame of {time_frame} days")
+    storyPrint(notFail)
+    date_time, value = dataFrameCreation(coin_type, currency_type, time_frame)
+    dataFrameBaby(date_time, value)
 
 
 
-#%%
+main()
