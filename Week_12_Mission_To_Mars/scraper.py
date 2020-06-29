fe1d2f431e1d6423bf8d4d7f463c49a5b159d740
John Vega <Disguyranger@gmail.com>
2020-06-29 13:06:56 -0400

    replaced

--- Week_12_Mission_To_Mars/scraper.py
+++
@@ -1,9 +0,0 @@
-from splinter import Browser as br
-from bs4 import BeautifulSoup as bs
-import requests as rq
-import pandas as pd
-import time
-
-def initializeBrowser():
-    executable_path ={'executable_path':'chromedriver.exe'}
-    return br('chrome', **executable_path, headless=False)
