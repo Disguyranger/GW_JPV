a6a261730a8a7e3d6dacc1613c94259d6c372617
Disguyranger <Disguyranger@gmail.com>
2020-07-14 13:08:38 -0400

    git

--- Week_12_Mission_To_Mars/mars_scraper.py
+++
@@ -1,9 +0,0 @@
-from splinter import Browser
-from bs4 import BeautifulSoup as bs
-import re, time
-
-executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path)
-
-def scraper():
-    
